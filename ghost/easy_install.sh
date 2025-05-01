886735838c74b2cfcff946cb3b9765be228a6c72
CodedPro <dev.codedpro@gmail.com>
2025-05-01 12:51:04 +0330

    Refactor installation scripts and add Dockerfile for environment setup

--- ghost/easy_install.sh
+++ ghost/easy_install.sh
@@ -1,14 +1,21 @@
-sudo apt-get install -qq libboost-all-dev build-essential gcc g++ libmysql++-dev libbz2-dev libgmp3-dev
+#!/bin/bash
+
+# Skip system dependency installation – handled by Dockerfile
+# sudo apt-get install -qq libboost-all-dev build-essential gcc g++ libmysql++-dev libbz2-dev libgmp3-dev
+
 cd src/bncsutil/src/bncsutil
 make
 sudo make install
+
 cd ../../../StormLib/stormlib
 make
 sudo make install
+
 cd ../../
 make
+
 cd ../
-mkdir bot
+mkdir -p bot
 cp src/ghost++ bot/
-cp config/* bot -R
-cp plugins bot/ -R 
+cp -R config/* bot/
+cp -R plugins bot/
