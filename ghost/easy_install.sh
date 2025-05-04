5bff91b9e88aeb9a86a1f2ffc975ab90c87110a8
CodedPro <dev.codedpro@gmail.com>
2025-05-04 14:29:56 +0330

    fix

--- ghost/easy_install.sh
+++ ghost/easy_install.sh
@@ -1,21 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# ------------ easy_install.sh ------------
+#   • Compiles GHost++ and assembles the runtime folder `bot/`
+#   • Assumes all system packages were installed by the Dockerfile.
+#   • Uses limited parallel jobs to avoid OOM in low‑memory Docker builds.
+set -e
 
-# Skip system dependency installation – handled by Dockerfile
-# sudo apt-get install -qq libboost-all-dev build-essential gcc g++ libmysql++-dev libbz2-dev libgmp3-dev
+JOBS=${MAKE_JOBS:-2}       # default 2 parallel jobs; override with MAKE_JOBS
 
+# 1) bncsutil
 cd src/bncsutil/src/bncsutil
-make
+make -j${JOBS}
 sudo make install
 
+# 2) StormLib
 cd ../../../StormLib/stormlib
-make
+make -j${JOBS}
 sudo make install
 
+# 3) Main GHost++ build
 cd ../../
-make
+make -j${JOBS}
 
+# 4) Assemble runtime directory
 cd ../
 mkdir -p bot
-cp src/ghost++ bot/
-cp -R config/* bot/
-cp -R plugins bot/
+cp src/ghost++           bot/
+cp -R config/*           bot/
+cp -R plugins            bot/
+
+echo "[easy_install] Build complete.  Run the bot with:  cd bot && ./ghost++"
